4f212614878595b8e511971cb627bc250472fdb6
lrsnider <lisarsnider@gmail.com>
2014-10-27 12:23:03 -0400

    image colum added to recipes model migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027152230) do
+ActiveRecord::Schema.define(version: 20141027161212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141027152230) do
     t.string  "source_url"
     t.integer "total_time"
     t.integer "servings"
+    t.string  "image"
   end
 
   create_table "users", force: true do |t|
