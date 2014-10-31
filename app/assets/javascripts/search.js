7b30ceb0ffa678169f1438364becaa8aa82d73b6
lrsnider <lisarsnider@gmail.com>
2014-10-31 08:32:20 -0400

    style recipe output

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
 //base GET url:
 //http://api.yummly.com/v1/api/recipes?_app_id=c8af64b9&_app_key=9b4d56a0e813fed791c590821441f01a&allowedIngredient[]=&allowedIngredient[]=cognac
-//Some of the parameters support passing multiple values; in these cases, we follow the AJAX convention: param[]=value1&param[]=value2&…
+//Some of the parameters support passing multiple values; in these cases, we follow the AJAX convention: param[]=value1&param[]=value2&...
 //Don’t forget to URL-encode parameter names and values (i.e., [ and ] become %5B and %5D).
 //
 
@@ -16,14 +16,17 @@ $('form#recipe_search').bind("ajax:success", function(event, data, status, xhr){
   // for each data item in json
   for (var i = 0; i < data.matches.length ; i++) {
   
-  var spaces = encodeURIComponent(value).replace(/%20/g,'+')
-  var google_link = encodeURIComponent('http://google.com/?q=" + ')
+  // var spaces = data.matches[i].recipeName().replace('/%20/g','+');
 
-  
-    var linkhttp://google.com/?q=' + 'data.matches[i].recipeName)');
+  var query = encodeURIComponent(data.matches[i].recipeName);
+  var link = 'http://google.com/?q=' + query;
+
+  if (!data.matches[i].smallImageUrls[0]){
+     data.matches[i].smallImageUrls[0] = ''
+  };
 
     $('.show-recipe').append(
-      '<li>' + '<a href="google_link">' + data.matches[i].recipeName + '</a>' + data.matches[i].recipeName + '<img src="' + data.matches[i].smallImageUrls[0] + '">' + '<p>' + 'rating: ' + 
+      '<li>' + '<a href=' + link + '>' + data.matches[i].recipeName + '</a><br /><img src="' + data.matches[i].smallImageUrls[0] + '">' + '<p>' + 'rating: ' + 
       data.matches[i].rating + '</p></li>'
       );
   };
