48ff55c308f83c160b14af57c80c961ef6a6b794
lrsnider <lisarsnider@gmail.com>
2014-11-13 14:58:01 -0500

    list-style-type: none

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -13,10 +13,9 @@ $(document).ready(function(){
 $('#submit').click(function(evt){
   evt.preventDefault();
 
-//loop thru the child elements in the chosen-choices ul 
-  
   var recipesUrl = 'http://api.yummly.com/v1/api/recipes?_app_id=c8af64b9&_app_key=9b4d56a0e813fed791c590821441f01a';
-
+  
+  //loop thru the child elements in the chosen-choices ul 
   var content = $('.search-choice');
   content.each(function(){
   
@@ -55,54 +54,13 @@ $('#submit').click(function(evt){
     });
 }); 
   
-
-  //get the text of each of those
-  //do same logic from lines 30-40 in JS, adding &allowedIngredient[]= before each URI encoded ingredient
-
-
-
-$('form#recipe_search').bind("ajax:success", function(event, data, status, xhr){
-  console.log(data);
-
-  // for each data item in json
-  for (var i = 0; i < data.matches.length ; i++) {
-  
-  // var spaces = data.matches[i].recipeName().replace('/%20/g','+');
-
-  var query = encodeURIComponent(data.matches[i].recipeName);
-  var link = 'http://google.com/?q=' + query + '#q=' + query;
-
-  if (!data.matches[i].smallImageUrls[0]){
-     data.matches[i].smallImageUrls[0] = ''
-  };
-
-    $('.show-recipe').append(
-     '<li>' + '<a href=' + link + '>' + data.matches[i].recipeName + '</a><br /><img src="' + data.matches[i].smallImageUrls[0] + '">' + '<p>' + 'rating: ' + 
-      data.matches[i].rating + '</p></li>'
-      );
-  };
-  //   generate <li> with data
-
-})
-
-//WHAT I WANT TO SHOW FROM RECIPE
+//WHAT I WANT TO SHOW FOR EACH RECIPE
 //recipeName
 //
 //numberOfServings
 //totalTimeInSeconds
 //sourceRecipeUrl
 
-  // $('.recipe-search-form').on('submit', function(event){
-  //   event.preventDefault();
-  //   // var term_chunk = '&allowedIngredient[]=';
-  //   // var search_term = $('.search_term').attr('data-id');
-  //   console.log(search_term);
-
-  //   var recipesUrl = 'http://api.yummly.com/v1/api/recipes?_app_id=c8af64b9&_app_key=9b4d56a0e813fed791c590821441f01a' + term_chunk + search_term
-
-  //   //loop thru li's
-  //     //convert into string pieces I need
-
-  //   console.log(recipesUrl);
+//var recipesUrl = 'http://api.yummly.com/v1/api/recipes?_app_id=c8af64b9&_app_key=9b4d56a0e813fed791c590821441f01a' + term_chunk + search_term
 })
 
