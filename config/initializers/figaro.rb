5d60f4d22c2e68e497e625eee74fddc5ca5264fc
lrsnider <lisarsnider@gmail.com>
2014-11-19 22:52:36 -0500

    figaro setup

---
+++ config/initializers/figaro.rb
@@ -0,0 +1,3 @@
+# config/initializers/figaro.rb 
+
+Figaro.require_keys("yummly_app_id", "yummly_key")
---
+++ config/initializers/yummly.rb
@@ -0,0 +1,4 @@
+#config/initializers/yummly.rb
+
+Yummly.app_id = ENV["pusher_app_id"]
+Yummly.key    = ENV["pusher_key"]
